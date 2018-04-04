d3d8c0a77e7b5f0a44a4d45d0c8b4383071c1fa7
Matvei <vitaliematvei@mail.ru>
2018-04-04 11:33:32 +0300

    FInal

---
+++ Matvei Vitalie/src/SendMail/MailList.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+/**Read file in the desktop**/
+const readFile = (path, opts = 'utf8') =>
+    new Promise((res, rej) => {
+        fs.readFile(path, opts, (err, data) => {
+            if (err)
+                rej(err)
+            else
+                res(data)
+        })
+    })
+
+/**Write file in EmailsSends: emails.json**/
+const writeFile = (path, sendEmail) =>
+    new Promise((res, rej) => {
+        fs.writeFile(path, sendEmail, (err, data) => {
+            if (err)
+                rej(err)
+            else
+                res(data)
+        })
+    })
+
+const readSendEmails = (path, opts = 'utf8') =>
+    new Promise((res, rej) => {
+        fs.readFile(path, opts, (err, data) => {
+            if (err)
+                rej(err)
+            else
+                res(data)
+        })
+    })
+
+module.exports = {
+    readFile,
+    writeFile,
+    readSendEmails
+}
